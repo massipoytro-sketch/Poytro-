6dce4bfb7da5637936cf0afec36bffe9104c6028
massipoytro-sketch <massipoytro@gmail.com>
2026-09-13 12:48:44 +0100

    style: load dedicated reference visual layer

--- src/main.jsx
+++ src/main.jsx
@@ -6,5 +6,6 @@ import './styles/global.css';
 import './styles/layout.css';
 import './styles/components.css';
 import './styles/responsive.css';
+import './styles/reference.css';
 
 createRoot(document.getElementById('root')).render(<App />);
