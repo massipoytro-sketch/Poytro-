f1bb301231543003a2953a084d9e5bcb91765451
massipoytro-sketch <massipoytro@gmail.com>
2026-09-13 13:10:36 +0100

    style: load GainiRen public page styles

--- src/main.jsx
+++ src/main.jsx
@@ -7,5 +7,6 @@ import './styles/layout.css';
 import './styles/components.css';
 import './styles/responsive.css';
 import './styles/reference.css';
+import './styles/public.css';
 
 createRoot(document.getElementById('root')).render(<App />);
