2019e8588b04698fc311eb15f286fd617cee6f4f
massipoytro-sketch <massipoytro@gmail.com>
2026-09-13 13:20:53 +0100

    design: load reference chest layer

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import './styles/layout.css';
 import './styles/components.css';
 import './styles/responsive.css';
 import './styles/reference.css';
+import './styles/chest.css';
 import './styles/public.css';
 
 createRoot(document.getElementById('root')).render(<App />);
